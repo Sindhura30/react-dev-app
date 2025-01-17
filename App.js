22b25d1502208bdd057c1a4d5ab23735da15c8c3
Sindhura Shetty <sindhura.shetty@lowes.com>
2025-01-17 10:34:52 +0530

    feat: first commit

--- App.js
+++ App.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return (
+        <h1>Testing</h1>
+    )
+}
+
+
+let root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App/>);
+
+
