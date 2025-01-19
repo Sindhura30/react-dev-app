62ccd508b14941c33945756293e1aa42ad7b1c47
Sindhura Shetty <sindhura.shetty@lowes.com>
2025-01-19 20:05:52 +0530

    feat: restuarant card

--- App.js
+++ App.js
@@ -2,9 +2,528 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+const Header = () => {
+    return (
+       <header>
+            <img src="https://www.creativefabrica.com/wp-content/uploads/2020/02/11/Food-Logo-Graphics-1-71-580x386.jpg" alt="logo" />
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Cart</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+const resList = [
+        {
+          "info": {
+            "id": "389995",
+            "name": "Chinese Wok",
+            "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
+            "locality": "16 A Main Rd",
+            "areaName": "Yelahanka New Town",
+            "costForTwo": "₹250 for two",
+            "cuisines": [
+              "Chinese",
+              "Asian",
+              "Tibetan",
+              "Desserts"
+            ],
+            "avgRating": 4.3,
+            "parentId": "61955",
+            "avgRatingString": "4.3",
+            "totalRatingsString": "3.1K+",
+            "sla": {
+              "deliveryTime": 24,
+              "lastMileTravel": 0.6,
+              "serviceability": "SERVICEABLE",
+              "slaString": "20-25 mins",
+              "lastMileTravelString": "0.6 km",
+              "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+              "nextCloseTime": "2025-01-19 00:00:00",
+              "opened": true
+            },
+            "badges": {
+              "imageBadges": [
+                {
+                  "imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Noodles.png",
+                  "description": "Delivery!"
+                }
+              ]
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+              "entityBadges": {
+                "imageBased": {
+                  "badgeObject": [
+                    {
+                      "attributes": {
+                        "description": "Delivery!",
+                        "imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Noodles.png"
+                      }
+                    }
+                  ]
+                },
+                "textBased": {
+
+                },
+                "textExtendedBadges": {
+
+                }
+              }
+            },
+            "aggregatedDiscountInfoV3": {
+              "header": "ITEMS",
+              "subHeader": "AT ₹149"
+            },
+            "differentiatedUi": {
+              "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+              "differentiatedUiMediaDetails": {
+                "lottie": {
+
+                },
+                "video": {
+
+                }
+              }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            },
+            "externalRatings": {
+              "aggregatedRating": {
+                "rating": "--"
+              }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+          },
+          "analytics": {
+            "context": "seo-data-299ba8cd-f189-4fa3-8e95-4dfe4cf236f5"
+          },
+          "cta": {
+            "link": "https://www.swiggy.com/city/bangalore/chinese-wok-16-a-main-rd-yelahanka-new-town-rest389995",
+            "type": "WEBLINK"
+          }
+        },
+        {
+          "info": {
+            "id": "53760",
+            "name": "Pizza Hut",
+            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+            "locality": "NES Service Road",
+            "areaName": "Yelahanka New Town",
+            "costForTwo": "₹350 for two",
+            "cuisines": [
+              "Pizzas"
+            ],
+            "avgRating": 4.4,
+            "parentId": "721",
+            "avgRatingString": "4.4",
+            "totalRatingsString": "8.8K+",
+            "sla": {
+              "deliveryTime": 25,
+              "lastMileTravel": 0.4,
+              "serviceability": "SERVICEABLE",
+              "slaString": "25-30 mins",
+              "lastMileTravelString": "0.4 km",
+              "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+              "nextCloseTime": "2025-01-19 00:00:00",
+              "opened": true
+            },
+            "badges": {
+
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+              "entityBadges": {
+                "imageBased": {
+
+                },
+                "textBased": {
+
+                },
+                "textExtendedBadges": {
+
+                }
+              }
+            },
+            "aggregatedDiscountInfoV3": {
+              "header": "₹300 OFF",
+              "subHeader": "ABOVE ₹1699",
+              "discountTag": "FLAT DEAL"
+            },
+            "differentiatedUi": {
+              "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+              "differentiatedUiMediaDetails": {
+                "lottie": {
+
+                },
+                "video": {
+
+                }
+              }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            },
+            "externalRatings": {
+              "aggregatedRating": {
+                "rating": "--"
+              }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+          },
+          "analytics": {
+            "context": "seo-data-299ba8cd-f189-4fa3-8e95-4dfe4cf236f5"
+          },
+          "cta": {
+            "link": "https://www.swiggy.com/city/bangalore/pizza-hut-nes-service-road-yelahanka-new-town-rest53760",
+            "type": "WEBLINK"
+          }
+        },
+        {
+          "info": {
+            "id": "84919",
+            "name": "Burger King",
+            "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/4ee8bc77-ca9f-41bd-a0f3-511c70902b91_84919.JPG",
+            "locality": "RMZ Galleraia",
+            "areaName": "Yelahanka",
+            "costForTwo": "₹350 for two",
+            "cuisines": [
+              "Burgers",
+              "American"
+            ],
+            "avgRating": 4.4,
+            "parentId": "166",
+            "avgRatingString": "4.4",
+            "totalRatingsString": "21K+",
+            "sla": {
+              "deliveryTime": 32,
+              "lastMileTravel": 1.1,
+              "serviceability": "SERVICEABLE",
+              "slaString": "30-35 mins",
+              "lastMileTravelString": "1.1 km",
+              "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+              "nextCloseTime": "2025-01-18 23:00:00",
+              "opened": true
+            },
+            "badges": {
+              "imageBadges": [
+                {
+                  "imageId": "Rxawards/_CATEGORY-Burger.png",
+                  "description": "Delivery!"
+                }
+              ]
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+              "entityBadges": {
+                "imageBased": {
+                  "badgeObject": [
+                    {
+                      "attributes": {
+                        "description": "Delivery!",
+                        "imageId": "Rxawards/_CATEGORY-Burger.png"
+                      }
+                    }
+                  ]
+                },
+                "textBased": {
+
+                },
+                "textExtendedBadges": {
+
+                }
+              }
+            },
+            "aggregatedDiscountInfoV3": {
+              "header": "60% OFF",
+              "subHeader": "UPTO ₹120"
+            },
+            "differentiatedUi": {
+              "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+              "differentiatedUiMediaDetails": {
+                "lottie": {
+
+                },
+                "video": {
+
+                }
+              }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            },
+            "externalRatings": {
+              "aggregatedRating": {
+                "rating": "--"
+              }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+          },
+          "analytics": {
+            "context": "seo-data-299ba8cd-f189-4fa3-8e95-4dfe4cf236f5"
+          },
+          "cta": {
+            "link": "https://www.swiggy.com/city/bangalore/burger-king-rmz-galleraia-yelahanka-rest84919",
+            "type": "WEBLINK"
+          }
+        },
+        {
+          "info": {
+            "id": "388936",
+            "name": "Big Bowl",
+            "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/22/deff0d02-ca1d-4ef9-9d62-b0cbeabf33d3_388936.JPG",
+            "locality": "Mother Dairy Road",
+            "areaName": "Yelahanka",
+            "costForTwo": "₹250 for two",
+            "cuisines": [
+              "North Indian",
+              "Chinese",
+              "Tibetan",
+              "Desserts"
+            ],
+            "avgRating": 4.3,
+            "parentId": "434792",
+            "avgRatingString": "4.3",
+            "totalRatingsString": "1.2K+",
+            "sla": {
+              "deliveryTime": 24,
+              "lastMileTravel": 0.6,
+              "serviceability": "SERVICEABLE",
+              "slaString": "20-25 mins",
+              "lastMileTravelString": "0.6 km",
+              "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+              "nextCloseTime": "2025-01-18 23:00:00",
+              "opened": true
+            },
+            "badges": {
+
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+              "entityBadges": {
+                "imageBased": {
+
+                },
+                "textBased": {
+
+                },
+                "textExtendedBadges": {
+
+                }
+              }
+            },
+            "aggregatedDiscountInfoV3": {
+              "header": "ITEMS",
+              "subHeader": "AT ₹149"
+            },
+            "differentiatedUi": {
+              "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+              "differentiatedUiMediaDetails": {
+                "lottie": {
+
+                },
+                "video": {
+
+                }
+              }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            },
+            "externalRatings": {
+              "aggregatedRating": {
+                "rating": "--"
+              }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+          },
+          "analytics": {
+            "context": "seo-data-299ba8cd-f189-4fa3-8e95-4dfe4cf236f5"
+          },
+          "cta": {
+            "link": "https://www.swiggy.com/city/bangalore/big-bowl-mother-dairy-road-yelahanka-rest388936",
+            "type": "WEBLINK"
+          }
+        },
+        {
+          "info": {
+            "id": "858822",
+            "name": "McDonald's",
+            "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/18/828420a7-e91b-42c6-898a-ffe743e83727_858822.jpg",
+            "locality": "Roopashree Building",
+            "areaName": "Yelahanka New Town",
+            "costForTwo": "₹400 for two",
+            "cuisines": [
+              "Burgers",
+              "Beverages",
+              "Cafe",
+              "Desserts"
+            ],
+            "avgRating": 4.4,
+            "parentId": "630",
+            "avgRatingString": "4.4",
+            "totalRatingsString": "1.2K+",
+            "sla": {
+              "deliveryTime": 20,
+              "lastMileTravel": 0.7,
+              "serviceability": "SERVICEABLE",
+              "slaString": "15-20 mins",
+              "lastMileTravelString": "0.7 km",
+              "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+              "nextCloseTime": "2025-01-18 23:59:00",
+              "opened": true
+            },
+            "badges": {
+              "imageBadges": [
+                {
+                  "imageId": "bolt/Bolt%20Listing%20badge@3x.png",
+                  "description": "bolt!"
+                },
+                {
+                  "imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Burger.png",
+                  "description": "Delivery!"
+                }
+              ]
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+              "entityBadges": {
+                "imageBased": {
+                  "badgeObject": [
+                    {
+                      "attributes": {
+                        "description": "bolt!",
+                        "imageId": "bolt/Bolt%20Listing%20badge@3x.png"
+                      }
+                    },
+                    {
+                      "attributes": {
+                        "description": "Delivery!",
+                        "imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Burger.png"
+                      }
+                    }
+                  ]
+                },
+                "textBased": {
+
+                },
+                "textExtendedBadges": {
+
+                }
+              }
+            },
+            "aggregatedDiscountInfoV3": {
+              "header": "₹50 OFF",
+              "subHeader": "ABOVE ₹299",
+              "discountTag": "FLAT DEAL"
+            },
+            "differentiatedUi": {
+              "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+              "differentiatedUiMediaDetails": {
+                "lottie": {
+
+                },
+                "video": {
+
+                }
+              }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            },
+            "externalRatings": {
+              "aggregatedRating": {
+                "rating": "--"
+              }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+          },
+          "analytics": {
+            "context": "seo-data-299ba8cd-f189-4fa3-8e95-4dfe4cf236f5"
+          },
+          "cta": {
+            "link": "https://www.swiggy.com/city/bangalore/mcdonalds-roopashree-building-yelahanka-new-town-rest858822",
+            "type": "WEBLINK"
+          }
+        }
+]
+
+const ResCard = (restaurant) => {
+    const { restaurant : { info } } = restaurant;
+    return (
+        <div className='res-wrapper'>
+            <div className='restuarant-badge'>
+                <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${info.cloudinaryImageId}`} />
+            </div>
+            <h3>{info.name}</h3>
+            <p className='rating'>
+                <span className='rating-icon'>
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" role="img" aria-hidden="true" strokeColor="rgba(2, 6, 12, 0.92)" fillColor="rgba(2, 6, 12, 0.92)">
+                    <circle cx="10" cy="10" r="9" fill="url(#StoreRating20_svg__paint0_linear_32982_71567)"></circle>
+                    <path d="M10.0816 12.865C10.0312 12.8353 9.96876 12.8353 9.91839 12.865L7.31647 14.3968C6.93482 14.6214 6.47106 14.2757 6.57745 13.8458L7.27568 11.0245C7.29055 10.9644 7.26965 10.9012 7.22195 10.8618L4.95521 8.99028C4.60833 8.70388 4.78653 8.14085 5.23502 8.10619L8.23448 7.87442C8.29403 7.86982 8.34612 7.83261 8.36979 7.77777L9.54092 5.06385C9.71462 4.66132 10.2854 4.66132 10.4591 5.06385L11.6302 7.77777C11.6539 7.83261 11.706 7.86982 11.7655 7.87442L14.765 8.10619C15.2135 8.14085 15.3917 8.70388 15.0448 8.99028L12.7781 10.8618C12.7303 10.9012 12.7095 10.9644 12.7243 11.0245L13.4225 13.8458C13.5289 14.2757 13.0652 14.6214 12.6835 14.3968L10.0816 12.865Z" fill="white"></path>
+                    <defs><linearGradient id="StoreRating20_svg__paint0_linear_32982_71567" x1="10" y1="1" x2="10" y2="19" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#21973B"></stop><stop offset="1" stop-color="#128540"></stop></linearGradient></defs></svg>
+                </span>
+                {info.avgRating}
+                <span className='delivery-time'><strong>{info.sla.slaString}</strong></span>
+            </p>
+            <p>{info.cuisines.join(', ')}</p>
+            <p>
+                <span>{info.areaName}</span>
+            </p>
+        </div>
+    )
+}
 const App = () => {
     return (
-        <h1>Testing</h1>
+        <div className="container">
+        <Header />
+        <div className='res-card'>
+            {resList.map((restaurant) => (
+                <ResCard key={restaurant.id} restaurant={restaurant} />
+            ))}
+            </div>
+        </div>
     )
 }
 
